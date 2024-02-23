1e152ed83d0f1e85d9aac960596977f8ab20ac42
giorgiskripnichenko <gskripnichenko@gh.ge>
2024-02-23 18:59:32 +0400

    ex

--- guess number.py
+++ guess number.py
@@ -7,11 +7,14 @@ userInput = None
 compNumber = random.randrange(0,50)
 while userInput != compNumber:
     print()
-    userInput = int(input("შეიყვანეთ რიცხვი \n"))
-    step += 1
-    if userInput > compNumber:
-        print('მაღალია')
-    else:
-        print('დაბალია')
+    try:
+        userInput = int(input("შეიყვანეთ რიცხვი \n"))
+        step += 1
+        if userInput > compNumber:
+            print('მაღალია')
+        else:
+            print('დაბალია')
+    except:
+        print("wrong input")
 print(f"თქვენ გამოიცანით რიცხვი {step} ჯერზე")
 
